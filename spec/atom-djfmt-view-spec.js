7468820828d0ef427d087906e02dc6b48cc8abdb
leetou <leetou@fnguide.com>
2016-05-20 15:23:55 +0900

    initial commit

---
+++ spec/atom-djfmt-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDjfmtView from '../lib/atom-djfmt-view';
+
+describe('AtomDjfmtView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
